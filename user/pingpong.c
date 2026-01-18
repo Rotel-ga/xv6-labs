490091e0c231ea8ac0ee13ab9eceddf2ed515a12
Rotel-ga <1119853326@qq.com>
2026-01-18 14:19:46 +0800

    完成第二个作业pingpong

---
+++ user/pingpong.c
@@ -0,0 +1,70 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+#define RD 0  //管道的读端
+#define WR 1  //管道的写端
+
+int main(int argc, char const *argv[])
+{
+    char buf = 'p';
+    
+    int fd_p2c[2];  //父进程->子进程
+    int fd_c2p[2];  //子进程->父进程
+
+    pipe(fd_p2c);
+    pipe(fd_c2p);
+
+    int pid = fork();
+    int exit_status = 0;
+
+    if(pid < 0){
+        fprintf(2, "fork() error!\n");
+        close(fd_c2p[RD]);
+        close(fd_c2p[WR]);
+        close(fd_p2c[RD]);
+        close(fd_p2c[WR]);
+        exit(1);
+    } else if(pid == 0){      //子进程
+        close(fd_c2p[RD]);
+        close(fd_p2c[WR]);
+
+        if(read(fd_p2c[RD], &buf, sizeof(char)) != sizeof(char)) {  //子进程向父进程读取
+            fprintf(2, "child read() error!\n");
+             exit_status = 1; //标记出错
+        } else {
+            fprintf(1, "%d: received ping\n", getpid());
+        }
+
+        if (write(fd_c2p[WR], &buf, sizeof(char)) != sizeof(char)) {    //子进程向父进程写入
+            fprintf(2, "child write() error!\n");
+            exit_status = 1;
+        }
+
+        close(fd_p2c[RD]);
+        close(fd_c2p[WR]);
+
+        exit(exit_status);
+    } else {     //父进程
+        close(fd_p2c[RD]);
+        close(fd_c2p[WR]);
+        //父进程向子进程发送一个字符
+        if (write(fd_p2c[WR], &buf, sizeof(char)) != sizeof(char)) {
+            fprintf(2, "parent write() error!\n");
+            exit_status = 1;
+        }
+
+        //父进程向子进程读取一个字符
+        if (read(fd_c2p[RD], &buf, sizeof(char)) != sizeof(char)) {
+            fprintf(2, "parent read() error!\n");
+            exit_status = 1; //标记出错
+        } else {
+            fprintf(1, "%d: received pong\n", getpid());
+        }
+
+        close(fd_p2c[WR]);
+        close(fd_c2p[RD]);
+
+        exit(exit_status);
+    }
+}
