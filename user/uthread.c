600cd230095aa12d8b4e053835a4cf5339b1bb8e
Rotel-ga <1119853326@qq.com>
2026-03-03 14:49:41 +0800

    实现用户态协程

--- user/uthread.c
+++ user/uthread.c
@@ -11,14 +11,35 @@
 #define MAX_THREAD  4
 
 
+// 线程切换需要保存的寄存器
+struct context
+{
+  uint64 ra;
+  uint64 sp;
+
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
+
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  struct context context;
 };
+
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+extern void thread_switch(struct context* old, struct context* new);
               
 void 
 thread_init(void)
@@ -59,10 +80,7 @@ thread_schedule(void)
     next_thread->state = RUNNING;
     t = current_thread;
     current_thread = next_thread;
-    /* YOUR CODE HERE
-     * Invoke thread_switch to switch from t to next_thread:
-     * thread_switch(??, ??);
-     */
+    thread_switch(&t->context, &next_thread->context);
   } else
     next_thread = 0;
 }
@@ -76,7 +94,13 @@ thread_create(void (*func)())
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+  
+  // 设定函数返回地址，thread_switch线程切换执行完后返回到ra,设置成线程函数func，切换后执行func
+  t->context.ra = (uint64)func;
+
+  // 设定栈指针
+  t->context.sp = (uint64)&t->stack + (STACK_SIZE - 1);
+
 }
 
 void 
