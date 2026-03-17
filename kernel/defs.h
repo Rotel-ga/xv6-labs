8aa128d5007913d5c1d0f62858af3d9fefa4c7a4
Rotel-ga <1119853326@qq.com>
2026-03-17 19:34:16 +0800

    实现了文件内存映射

--- kernel/defs.h
+++ kernel/defs.h
@@ -140,6 +140,10 @@ int             fetchstr(uint64, char*, int);
 int             fetchaddr(uint64, uint64*);
 void            syscall();
 
+// sysfile.c
+struct vma*     findvma(struct proc*, uint64);
+int             vmaalloc(uint64);
+
 // trap.c
 extern uint     ticks;
 void            trapinit(void);
@@ -171,6 +175,7 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+void            vmaunmap(pagetable_t, uint64, uint64, struct vma*);
 
 // plic.c
 void            plicinit(void);
